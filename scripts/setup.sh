af1a3ad0950af1d62cc9623afac7dfc7e2c1682b
Darren Ldl <darrenldldev@gmail.com>
2019-04-18 21:00:50 +1000

    Updated author details

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Author : darrenldl <dldldev@yahoo.com>
+# Author : Darren Ldl <darrenldldev@gmail.com>
 #
 # License:
 # This is free and unencumbered software released into the public domain.
