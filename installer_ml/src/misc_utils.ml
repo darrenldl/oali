4c716fe6d4b86f4c07c265e3856a3cf532b75350
Darren Ldl <darrenldldev@gmail.com>
2019-09-07 14:47:18 +1000

    Progress on misc_utils

---
+++ installer_ml/src/misc_utils.ml
@@ -0,0 +1,6 @@
+let do_and_ask_retry_if_fail ?(action_name = "") (f : unit -> ('a, 'b) result) =
+  let rec aux f =
+    match f () with
+    | Ok x -> Ok x
+    | Error x ->
+      Printf.printf "Action %s failed, do you want to retry?" action_name;
