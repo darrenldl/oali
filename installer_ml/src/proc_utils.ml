c06715bcaba4952839836517f59cc8c974d230cc
Darren Ldl <darrenldldev@gmail.com>
2019-09-07 16:11:08 +1000

    Removing use of lwt

--- installer_ml/src/proc_utils.ml
+++ installer_ml/src/proc_utils.ml
@@ -1,26 +1,42 @@
 type process_status = Unix.process_status
 
 type exec_result =
-  { command : string array
+  { prog : string
+  ; args : string array
   ; status : process_status
-  ; stdout : string }
+  ; stdout : string list }
 
 let exec_result_is_ok res = res.status = WEXITED 0
 
-let exec command : (exec_result, exec_result) result Lwt.t =
-  let p = Lwt_process.open_process_full ("", command) in
-  let%lwt status = p#status in
-  let%lwt stdout = Lwt_io.read p#stdout in
-  let res = {command; status; stdout} in
-  if exec_result_is_ok res then Lwt.return (Ok res) else Lwt.return (Error res)
+let input_all_lines in_chan =
+  let rec aux in_chan acc =
+    let stop, acc =
+      try
+        false, input_line in_chan :: acc
+      with
+      | End_of_file -> true, acc
+    in
+    if stop then
+      acc
+    else
+      aux in_chan acc
+  in
+  List.rev (aux in_chan [])
 
-let exec_with_stdin command :
-  Lwt_io.output_channel * (unit -> (exec_result, exec_result) result Lwt.t) =
-  let p = Lwt_process.open_process_full ("", command) in
-  ( p#stdin
+let exec prog args : (exec_result, exec_result) result =
+  let stdout_chan = Unix.open_process_args_in prog args in
+  let stdout = input_all_lines stdout_chan in
+  let status = Unix.close_process_in stdout_chan in
+  let res = {prog; args; status; stdout} in
+  if exec_result_is_ok res then Ok res else Error res
+
+let exec_with_stdin prog args :
+  out_channel * (unit -> (exec_result, exec_result) result Lwt.t) =
+  let (stdout_chan, stdin_chan) = Unix.open_process_args prog args in
+  ( stdin_chan
   , fun () ->
-    let%lwt status = p#status in
-    let%lwt stdout = Lwt_io.read p#stdout in
-    let res = {command; status; stdout} in
+    let stdout = input_all_lines stdout_chan in
+    let status = Unix.close_process (stdout_chan, stdin_chan) in
+    let res = {prog; args; status; stdout} in
     if exec_result_is_ok res then Lwt.return (Ok res)
     else Lwt.return (Error res) )
