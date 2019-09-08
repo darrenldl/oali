d9702127014843b2b6fd9d45498afeb46262598d
Darren Ldl <darrenldldev@gmail.com>
2019-09-08 21:09:40 +1000

    Updated test code

--- installer_ml/src/installer.ml
+++ installer_ml/src/installer.ml
@@ -1,3 +1,9 @@
 let () =
-  try Proc_utils.exec "ls"
+  try
+    let stdin, f = Proc_utils.exec_ret_with_stdin "ls" in
+    output_string stdin "abcd";
+    let res = f () in
+    print_endline "done";
+    List.iter print_endline
+    res.stdout;
   with Proc_utils.Exec_fail r -> print_endline (Proc_utils.report_failure r)
