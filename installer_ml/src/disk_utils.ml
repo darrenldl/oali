af778f867d847c397f328389add6f46f64f1d1b8
Darren Ldl <darrenldldev@gmail.com>
2019-09-15 21:55:29 +1000

    Code refactoring

--- installer_ml/src/disk_utils.ml
+++ installer_ml/src/disk_utils.ml
@@ -1,9 +1,3 @@
-module DiskPartMap = Map.Make (struct
-    type t = string
-
-    let compare = compare
-  end)
-
 let list_parts () =
   let dir = "/dev/disk/by-partuuid/" in
   let uuids = Sys.readdir dir |> Array.to_list in
@@ -19,31 +13,3 @@ let part_to_disk s = String_utils.strip_tail_num s
 let list_disks () =
   let parts = list_parts () in
   parts |> List.map part_to_disk |> List.sort_uniq compare
-
-let group_parts_by_disks_map parts =
-  parts
-  |> List.map (fun part -> (part_to_disk part, part))
-  |> List.fold_left
-    (fun tbl (disk, part) ->
-       match DiskPartMap.find_opt disk tbl with
-       | None ->
-         DiskPartMap.add disk [part] tbl
-       | Some l ->
-         DiskPartMap.add disk (part :: l) tbl)
-    DiskPartMap.empty
-
-let group_parts_by_disks parts =
-  group_parts_by_disks_map parts
-  |> DiskPartMap.to_seq |> List.of_seq
-  |> List.sort (fun (k1, _) (k2, _) -> compare k1 k2)
-  |> List.map (fun (k, l) -> (k, List.sort compare l))
-
-let get_part_from_tree tree disk part =
-  let _, l = List.nth tree disk in
-  List.nth l part
-
-let remove_part_from_tree tree disk part =
-  List.mapi
-    (fun i (k, l) ->
-       if i = disk then (k, Misc_utils.list_no_nth l part) else (k, l))
-    tree
--- installer_ml/src/installer.ml
+++ installer_ml/src/installer.ml
@@ -64,37 +64,35 @@ let () =
         print_int choice; config
       | Sys_part_plus_boot_plus_maybe_EFI ->
         let parts = Disk_utils.list_parts () in
-        let disk_part_tree = Disk_utils.group_parts_by_disks parts in
-        let disk_part_tree, sys_part =
-          let disk, part =
+        let disk_part_tree = Disk_part_tree.of_parts parts in
+        let disk_part_tree, sys_part_path =
+          let disk_index, part_index =
             pick_choice_grouped
               ~first_header:"Select disk containing the system partition"
               ~second_header:"Select system partition" disk_part_tree
           in
-          ( Disk_utils.remove_part_from_tree disk_part_tree disk part
-          , Disk_utils.get_part_from_tree disk_part_tree disk part )
+          ( Disk_part_tree.remove_part_by_index ~disk_index ~part_index disk_part_tree
+          , Disk_part_tree.get ~disk_index ~part_index disk_part_tree )
         in
-        print_endline sys_part;
-        let disk_part_tree, boot_part =
-          let disk, part =
+        let disk_part_tree, boot_part_path =
+          let disk_index, part_index =
             pick_choice_grouped
               ~first_header:"Select disk containing the boot partition"
               ~second_header:"Select boot partition" disk_part_tree
           in
-          ( Disk_utils.remove_part_from_tree disk_part_tree disk part
-          , Disk_utils.get_part_from_tree disk_part_tree disk part )
+          ( Disk_part_tree.remove_part_by_index ~disk_index ~part_index disk_part_tree
+          , Disk_part_tree.get ~disk_index ~part_index disk_part_tree)
         in
-        print_endline boot_part;
-        let _efi_part =
+        let efi_part_path =
           if Sys.file_exists "/sys/firmware/efi" then (
             print_boxed_msg
               "System is in EFI mode, launching EFI partition selection menu";
-            let disk, part =
+            let disk_index, part_index =
               pick_choice_grouped
                 ~first_header:"Select disk containing the EFI partition"
                 ~second_header:"Select EFI partition" disk_part_tree
             in
-            Some (Disk_utils.get_part_from_tree disk_part_tree disk part) )
+            Some (Disk_part_tree.get ~disk_index ~part_index disk_part_tree) )
           else (
             print_boxed_msg
               "System is in BIOS mode, EFI partition selection skipped";
