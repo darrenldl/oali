ef2861c5403f9b8b4ca8f549a2db17c7f52b0f53
Darren Ldl <darrenldldev@gmail.com>
2019-09-15 22:43:32 +1000

    Fixed gen_rand_string

--- installer_ml/src/rand_utils.ml
+++ installer_ml/src/rand_utils.ml
@@ -2,4 +2,5 @@ let gen_rand_string =
   let initialised = ref false in
   fun ~len ->
     if not !initialised then Nocrypto_entropy_unix.initialize ();
+    initialised := true;
     Nocrypto.Rng.generate len
