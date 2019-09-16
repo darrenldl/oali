2c294b5e0f0f2fb8cc935329464975bbb810b62a
Darren Ldl <darrenldldev@gmail.com>
2019-09-16 16:50:21 +1000

    Reverted

--- installer_ml/src/string_utils.ml
+++ installer_ml/src/string_utils.ml
@@ -6,7 +6,7 @@ let strip_tail_num s =
   let pos = ref None in
   String.iteri
     (fun i c ->
-       if Base.Char.is_digit c then
+       if Core_kernel.Char.is_digit c then
          pos := match !pos with None -> Some i | Some x -> Some x)
     s;
   match !pos with None -> s | Some pos -> String.sub s 0 pos
@@ -16,7 +16,7 @@ let get_tail_num s =
   let pos = ref None in
   String.iteri
     (fun i c ->
-       if Base.Char.is_digit c then
+       if Core_kernel.Char.is_digit c then
          pos := match !pos with None -> Some i | Some x -> Some x)
     s;
   match !pos with
