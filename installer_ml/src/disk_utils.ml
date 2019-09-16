79c9b466cb5df6d07dbb00a08976f811bcb82f13
Darren Ldl <darrenldldev@gmail.com>
2019-09-16 22:38:09 +1000

    Fixed list_disks and disk_size

--- installer_ml/src/disk_utils.ml
+++ installer_ml/src/disk_utils.ml
@@ -1,21 +1,29 @@
-let list_parts () =
-  let dir = "/dev/disk/by-partuuid/" in
-  let uuids = Sys.readdir dir |> Array.to_list in
-  uuids
+let list_disk_block_devs () =
+  let dir = "/dev/disk/by-path/" in
+  let paths = Sys.readdir dir |> Array.to_list in
+  paths
   |> List.map (fun s -> dir ^ s)
   |> List.map Unix.readlink
   |> List.map (String_utils.tail 6)
   |> List.filter (fun s -> String.sub s 0 2 <> "dm")
+  |> List.filter (fun s -> String.sub s 0 2 <> "sr")
   |> List.map (fun s -> "/dev/" ^ s)
+
+let list_parts () =
+  list_disk_block_devs ()
+  |> List.filter (fun s -> match String_utils.get_tail_num s with
+      | Some _ -> true
+      | None -> false)
 
 let part_to_disk s = String_utils.strip_tail_num s
 
 let list_disks () =
-  let parts = list_parts () in
-  parts |> List.map part_to_disk |> List.sort_uniq compare
+  list_disk_block_devs ()
+  |> List.filter (fun s -> match String_utils.get_tail_num s with
+      | Some _ -> false
+      | None -> true)
 
 let disk_size disk =
-  let disk = String_utils.strip_prefix ~prefix:"/dev/" disk in
-  let ic = open_in (Printf.sprintf "/sys/block/%s/size" disk) in
-  let size_str = input_line ic in
-  close_in ic; int_of_string size_str
+  let res = Proc_utils.exec_ret (Printf.sprintf "blockdev --getsize64 %s" disk) in
+  let size_str = List.hd res.stdout in
+  int_of_string size_str
