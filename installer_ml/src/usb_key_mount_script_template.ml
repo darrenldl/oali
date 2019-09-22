e37819ce8c8491f4c6d605237aa6ada04333d118
Darren Ldl <darrenldldev@gmail.com>
2019-09-23 00:45:06 +1000

    Updated template

--- installer_ml/src/usb_key_mount_script_template.ml
+++ installer_ml/src/usb_key_mount_script_template.ml
@@ -1,8 +1,8 @@
 let gen ~use_encrypt ~is_efi_mode ~esp_uuid ~boot_uuid =
   Printf.sprintf
     {|#!/bin/bash
-encrypt=%s
-efi_mode=%s
+encrypt=%b
+efi_mode=%b
 usb_key_esp_uuid=%s
 usb_key_boot_uuid=%s
 mapper_name_boot=%s
@@ -10,18 +10,18 @@ mapper_name_boot=%s
 while true; do
   echo "Looking for encrypted boot"
 
-  if [ -b /dev/disk/by-uuid/"$usb_key_boot_uuid" ]; then
-    echo "Unlocking boot partition"
-    if $encrypt; then
+  if $encrypt; then
+    if [ -b /dev/disk/by-uuid/"$usb_key_boot_uuid" ]; then
+      echo "Unlocking boot partition"
       cryptsetup open --type luks /dev/disk/by-uuid/"$usb_key_boot_uuid" "$mapper_name_boot"
       if [[ $? == 0 ]]; then
         break
       else
         :
       fi
+    else
+      echo "Boot partition not found"
     fi
-  else
-    echo "Boot partition not found"
   fi
 
   echo -n "Do you want to exit the script? y/N : "
@@ -65,7 +65,6 @@ fi
 
 echo "USB key mounted successfully"
 |}
-    (if use_encrypt then "true" else "false")
-    (if is_efi_mode then "true" else "false")
+    use_encrypt is_efi_mode
     (Option.value ~default:"" esp_uuid)
     boot_uuid Config.boot_mapper_name
