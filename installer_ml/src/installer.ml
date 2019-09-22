44af572e299e0031e96346908fd1f51fc3f60a38
Darren Ldl <darrenldldev@gmail.com>
2019-09-22 18:39:49 +1000

    Path fix

--- installer_ml/src/installer.ml
+++ installer_ml/src/installer.ml
@@ -568,20 +568,28 @@ let () =
       config);
   reg ~name:"Copying useradd helper scripts" (fun config ->
       let cwd = Sys.getcwd () in
-      let dst_path = Config.sys_mount_point ^ Config.llsh_files_dir_path in
+      let dst_path =
+        concat_file_names [Config.sys_mount_point; Config.llsh_files_dir_path]
+      in
       FileUtil.cp
-        [ Printf.sprintf "%s/scripts/%s" cwd
-            Config.useradd_helper_as_powerful_name ]
+        [ concat_file_names
+            [ cwd
+            ; Config.repo_name
+            ; "scripts"
+            ; Config.useradd_helper_as_powerful_name ] ]
         dst_path;
       FileUtil.cp
-        [ Printf.sprintf "%s/scripts/%s" cwd
-            Config.useradd_helper_as_powerful_name ]
+        [ concat_file_names
+            [ cwd
+            ; Config.repo_name
+            ; "scripts"
+            ; Config.useradd_helper_restricted_name ] ]
         dst_path;
       Unix.chmod
-        (Printf.sprintf "%s/%s" dst_path Config.useradd_helper_as_powerful_name)
+        (concat_file_names [dst_path; Config.useradd_helper_as_powerful_name])
         0o660;
       Unix.chmod
-        (Printf.sprintf "%s/%s" dst_path Config.useradd_helper_restricted_name)
+        (concat_file_names [dst_path; Config.useradd_helper_restricted_name])
         0o660;
       config);
   reg ~name:"Ask if set up SaltStack" (fun config ->
