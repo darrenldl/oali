69e9199374e696243ff080abf978c59bf7f14907
Darren Ldl <darrenldldev@gmail.com>
2019-09-23 00:45:14 +1000

    Added USB key unmount script template

---
+++ installer_ml/src/usb_key_unmount_script_template.ml
@@ -0,0 +1,24 @@
+let gen ~use_encrypt ~is_efi_mode =
+  Printf.sprintf
+    {|
+#!/bin/bash
+encrypt=%b
+efi_mode=%b
+mapper_name_boot=%s
+
+if $efi_mode; then
+  echo "Umounting EFI partition"
+  umount /efi
+fi
+
+echo "Unmounting boot partition"
+umount /boot
+
+if $encrypt; then
+  echo "Closing boot partition"
+  cryptsetup close /dev/mapper/"$mapper_name_boot"
+fi
+
+echo "USB key unmounted successfully"
+|}
+    use_encrypt is_efi_mode Config.boot_mapper_name
