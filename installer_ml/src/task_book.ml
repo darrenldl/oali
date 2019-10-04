744e56caf63b1de5383537f19cd4d093f7977c96
Darren Ldl <darrenldldev@gmail.com>
2019-10-04 18:35:31 +1000

    Progress on task_book upgrade

--- installer_ml/src/task_book.ml
+++ installer_ml/src/task_book.ml
@@ -2,6 +2,8 @@ open Sexplib.Std
 
 type task = Task_config.t -> Task_config.t
 
+exception End_install
+
 type task_result =
   | Not_run
   | Okay
@@ -30,81 +32,102 @@ type t =
   ; mutable tasks_to_run : task_record array option
   ; mutable cur_task : int option }
 
-let make config = {config; task_queue = []; tasks_to_run = None; cur_task = None }
+let task_result_to_string res =
+  match res with
+  | Not_run ->
+    "Not run"
+  | Okay ->
+    "Finished successfully"
+  | Failed ->
+    "Failed"
+  | Skipped ->
+    "Skipped"
+
+let make config =
+  {config; task_queue = []; tasks_to_run = None; cur_task = None}
 
 let register task_book ~name task =
   task_book.task_queue <-
-    {name; task; stats = {result = Not_run; exec_count = 0}} :: task_book.task_queue
+    {name; task; stats = {result = Not_run; exec_count = 0}}
+    :: task_book.task_queue
 
 let finalize task_book =
   assert (task_book.tasks_to_run = None);
   task_book.tasks_to_run <- Some (Array.of_list task_book.task_queue)
 
+let print_task_list task_book =
+  let tasks_to_run = Option.get task_book.tasks_to_run in
+  print_endline "Task list";
+  Array.iteri
+    (fun i task_record -> Printf.printf "%5d    %s\n" i task_record.name)
+    tasks_to_run
+
+let pick_task task_book =
+  let tasks_to_run = Option.get task_book.tasks_to_run in
+  let choices =
+    tasks_to_run |> Array.to_list
+    |> List.map (fun record ->
+        Printf.sprintf "%s    %s" record.name
+          (task_result_to_string record.stats.result))
+  in
+  Misc_utils.pick_choice ~header:"Tasks" choices
+
+let rec run_single_task task_book task_record : unit =
+  let name = task_record.name in
+  let task = task_record.task in
+  Proc_utils.clear ();
+  print_endline name;
+  for _ = 0 to String.length name - 1 do
+    print_string "="
+  done;
+  print_newline ();
+  print_newline ();
+  let succeeded, new_config =
+    try
+      let config = task task_book.config in
+      print_newline (); (true, config)
+    with
+    | Proc_utils.Exec_fail r ->
+      print_endline (Proc_utils.report_failure r);
+      (false, task_book.config)
+    | Failure msg ->
+      Printf.printf "Failure : %s\n" msg;
+      (false, task_book.config)
+    | Sys_error msg ->
+      Printf.printf "Sys_error : %s\n" msg;
+      (false, task_book.config)
+  in
+  if not succeeded then (
+    let choices =
+      [("Retry", Retry); ("Skip", Skip); ("End install", End_install)]
+    in
+    let choice_index =
+      Misc_utils.pick_choice ~confirm:true (List.map (fun (x, _) -> x) choices)
+    in
+    let choice = List.nth choices choice_index |> fun (_, x) -> x in
+    match choice with
+    | Retry ->
+      run_single_task task_book task_record
+    | Skip ->
+      task_record.stats.result <- Skipped
+    | End_install ->
+      task_record.stats.result <- Failed;
+      raise End_install )
+  else task_book.config <- new_config
+
 let run task_book =
   finalize task_book;
-  let rec run_single_task task_record : unit =
-    let name = task_record.name in
-    let task = task_record.task in
-    Proc_utils.clear ();
-    print_endline name;
-    for _ = 0 to String.length name - 1 do
-      print_string "="
-    done;
-    print_newline ();
-    print_newline ();
-    let succeeded, new_config =
-      try
-        let config = task task_book.config in
-        print_newline (); (true, config)
-      with
-      | Proc_utils.Exec_fail r ->
-        print_endline (Proc_utils.report_failure r);
-        (false, task_book.config)
-      | Failure msg ->
-        Printf.printf "Failure : %s\n" msg;
-        (false, task_book.config)
-      | Sys_error msg ->
-        Printf.printf "Sys_error : %s\n" msg;
-        (false, task_book.config)
-    in
-    if not succeeded then
-      let choices =
-        [("retry", Retry); ("skip", Skip); ("end install", End_install)]
-      in
-      let choice_index =
-        Misc_utils.pick_choice ~confirm:true
-          (List.map (fun (x, _) -> x) choices)
-      in
-      let choice = List.nth choices choice_index |> fun (_, x) -> x in
-      match choice with
-      | Retry ->
-        run_single_task task_record
-      | Skip ->
-        task_record.stats.result <- Skipped
-      | End_install ->
-        task_record.stats.result <- Failed
-    else (
-      task_book.config <- new_config;
-      Queue.push (name, task) task_book.finished;
-      aux task_book None )
-  in
-  Array.iteri (fun cur_task task ->
-      task_book.cur_task <- Some cur_task;
-    ) task_book.tasks_to_run
-  let rec aux task_book (retry : (string * task) option) =
-    let to_run =
-      match retry with
-      | Some x ->
-        Some x
-      | None ->
-        Queue.take_opt task_book.to_run
-    in
-    match to_run with
-    | None ->
-      ()
-    | Some (name, task) ->
-          (* | _ ->
-           *   print_endline "Unknown failure";
-           *   (false, task_book.config) *)
-  in
-  aux task_book None
+  let tasks_to_run = Option.get task_book.tasks_to_run in
+  Sys.set_signal Sys.sigint
+    (Sys.Signal_handle
+       (fun _ ->
+          print_endline "Interrupted received";
+          print_endline "Terminating installation";
+          raise End_install));
+  try
+    Array.iteri
+      (fun cur_task task_record ->
+         task_book.cur_task <- Some cur_task;
+         run_single_task task_book task_record)
+      tasks_to_run
+  with End_install -> print_endline "Installation ended as requested"
