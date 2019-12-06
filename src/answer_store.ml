05a696ae8d3352471bf03da48850bd72ce26d0a3
Darren Ldl <darrenldldev@gmail.com>
2019-12-06 17:25:48 +1100

    Adding answer store

---
+++ src/answer_store.ml
@@ -0,0 +1,32 @@
+type t = (string, string) Hashtbl.t
+
+let to_json (t : t) : Yojson.Basic.t =
+  let l =
+    t
+    |> Hashtbl.to_seq
+    |> Seq.map (fun (k, v) ->
+        (k, `String v)
+      )
+    |> List.of_seq
+  in
+  `Assoc l
+
+let to_file fname (t : t) =
+  Yojson.Basic.to_file fname (to_json t)
+
+let of_json (x : Yojson.Basic.t) : t =
+  match x with
+  | `Assoc l ->
+    let t = Hashtbl.create 100 in
+    List.iter (fun (k, v) ->
+        match v with
+        | `String v ->
+          Hashtbl.add t k v
+        | _ -> failwith "Unexpected case"
+      ) l;
+    t
+  | _ -> failwith "Unexpected case"
+
+let from_file fname : t =
+  Yojson.Basic.from_file fname
+  |> of_json
