ad6187b0bf0efa13f7c341e325e7faa97785787f
Darren Ldl <darrenldldev@gmail.com>
2019-12-08 22:04:34 +1100

    Removed unused commented code

--- src/misc_utils.ml
+++ src/misc_utils.ml
@@ -1,11 +1,3 @@
-(* let do_and_ask_retry_if_fail ?(action_name = "") (f : unit -> ('a, 'b) result) =
- *   let rec aux f =
- *     match f () with
- *     | Ok x -> Ok x
- *     | Error x ->
- *       Printf.printf "Action %s failed, do you want to retry?" action_name
- *   in *)
-
 type 'a retry =
   | Stop of 'a
   | Retry
